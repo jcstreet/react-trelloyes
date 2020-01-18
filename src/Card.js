e2d1a14f7fa0dc261de4937b42d11da4d58adfdf
jcstreet <jackiecstreet@gmail.com>
2020-01-18 12:35:23 -0500

    first commit

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Card.css';
+
+function Card(props) {
+    return (
+        <div className='Card'>
+            <button
+                onClick={() => props.onDeleteCard(props.id)}
+                type='button'
+            >
+                delete
+            </button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )   
+}
+
+Card.propTypes = {
+    onDeleteCard: () => {}
+}
+
+export default Card;
---
+++ src/List.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './List.css';
+import Card from './Card.js';
+import './Store.js';
+
+function List(props) {
+    return (
+        <section className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+            </header>
+            <div className='List-cards'>   
+                {props.cards.map((card) => 
+                    <Card
+                        key={card.id}
+                        id={card.id}
+                        title={card.title}
+                        content={card.content}
+                        onDeleteCard={props.onDeleteCard}
+                    />
+                )}
+                <button
+                    type='button'
+                    className='List-add-button'
+                    onClick={() => props.onAddCard(props.id)}
+                >
+                    + Add Random Card
+                </button>
+            </div>
+        </section>
+    )
+}
+
+List.defaultProps = {
+    onAddCard: () => {},
+  }
+
+export default List;
